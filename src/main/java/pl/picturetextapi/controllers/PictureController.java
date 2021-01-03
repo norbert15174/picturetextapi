f79049685980c3a2dd874c6acac49bb5f89d3649
Faron Norbert <faronnorbertkrk@gmail.com>
2021-01-03 13:08:14 +0100

    added gcp vision

--- src/main/java/pl/picturetextapi/controllers/PictureController.java
+++ src/main/java/pl/picturetextapi/controllers/PictureController.java
@@ -3,8 +3,11 @@ package pl.picturetextapi.controllers;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,7 +18,16 @@ import java.io.IOException;
 @RequestMapping("/post")
 public class PictureController {
 
- //   @GetMapping
+
+    private CloudVisionTemplate cloudVisionTemplate;
+    private ResourceLoader resourceLoader;
+    @Autowired
+    public PictureController(CloudVisionTemplate cloudVisionTemplate, ResourceLoader resourceLoader) {
+        this.cloudVisionTemplate = cloudVisionTemplate;
+        this.resourceLoader = resourceLoader;
+    }
+
+    //   @GetMapping
     @PostMapping
     public String uploadFile(@RequestParam("file") MultipartFile multipartFile){
         ITesseract instance = new Tesseract();
@@ -42,6 +54,15 @@ public class PictureController {
 
 
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void asd(){
+        String text = this.cloudVisionTemplate.extractTextFromImage(resourceLoader.getResource("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQui3lNoQWGNN50Q2AwoiAnXnel3IpMx6auTA&usqp=CAU"));
+        System.out.println(text);
+
+    }
+
+
 
 
 }
